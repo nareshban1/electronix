e36f8c805ba1e27e67b9d54bcb7463171e2d1e38
Naresh Ban <np03a170060@heraldcollege.edu.np>
2022-02-04 09:21:26 +0545

    feat: Open Cart on Cart Icon click on Navbar

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -1,14 +1,25 @@
 import React from "react";
 import { Container } from "../../assets/styles/commonStyles.css";
 import { CartLogo, Logo, NavbarContainer, NavbarFlex } from "./Navbar.css";
-
+import { useSelector, useDispatch } from "react-redux";
+import { RootState } from "../../store";
+import { showCart } from "../../store/modules/ToggleCart/toggleActions";
 const Navbar = () => {
+  const viewCart = useSelector((state: RootState) => state.toggleCart.viewCart);
+  const dispatch = useDispatch();
+
+  const displayCart = () => {
+    if (viewCart === false) {
+      dispatch(showCart());
+    }
+  };
+
   return (
     <NavbarContainer>
       <Container>
         <NavbarFlex>
           <Logo>Electronix</Logo>
-          <CartLogo />
+          <CartLogo onClick={displayCart} />
         </NavbarFlex>
       </Container>
     </NavbarContainer>
