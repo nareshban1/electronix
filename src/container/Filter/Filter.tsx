c5cb4d559b22a27aae47bcf3c5e8d8ba2e32669a
Naresh Ban <np03a170060@heraldcollege.edu.np>
2022-02-04 16:26:22 +0545

    refactor: Removed range slider

--- src/container/Filter/Filter.tsx
+++ src/container/Filter/Filter.tsx
@@ -1,8 +1,6 @@
 import React from "react";
-import RangeSlider from "../../components/RangeSlider/RangeSlider";
 import {
   CategoriesContainer,
-  Categories,
   FilterContainer,
   SubCategories,
   SubCategoriesContainer,
@@ -28,13 +26,7 @@ const Filter = ({
   return (
     <FilterContainer>
       <FilterHeader>Price Range</FilterHeader>
-      <RangeSlider
-        min={0}
-        max={100000}
-        onChange={({ min, max }: { min: number; max: number }) =>
-          console.log(`min = ${min}, max = ${max}`)
-        }
-      />
+
       <FilterHeader>Date</FilterHeader>
       <FilterHeader>
         Categories{" "}
